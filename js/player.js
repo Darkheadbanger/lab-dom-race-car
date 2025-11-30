faf267bace223363d76f98742bcabc2b3c76e2e3
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-01 03:46:29 +0700

    feat(player): add constructor to initialize player properties and DOM element

--- js/player.js
+++ js/player.js
@@ -1,3 +1,19 @@
 class Player {
-    
+  constructor(gameScreen, left, top, width, height, imgSrc) {
+    this.gameScreen = gameScreen;
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.element = document.createElement("img");
+    this.element.src = imgSrc;
+    this.element.position = "absolute";
+    this.element.style.width = `${width}`;
+    this.element.style.height = `${height}`;
+    this.element.style.left = `${left}`;
+    this.element.style.top = `${top}`;
+    this.gameScreen.appendChild(this.element);
+  }
 }
