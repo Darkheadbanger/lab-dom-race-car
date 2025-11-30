095858d623680f6c5445a051c15ffba341258f69
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-01 03:29:15 +0700

    feat(game): implement Game class with constructor and game loop functionality feat(player): add Player class skeleton chore(index): include player.js in HTML

--- js/game.js
+++ js/game.js
@@ -1,3 +1,54 @@
 class Game {
-    // code to be added
+  // code to be added
+  constructor(
+    startScreen,
+    gameScreen,
+    gameEndScreen,
+    player,
+    height,
+    width,
+    obstacles,
+    score,
+    lives,
+    gameIsOver,
+    gameIntervalId,
+    gameLoopFrequency
+  ) {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrequency = Math.round(1e6 / 60) / 1000; // More precise 60 fps
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    
+    // Hide start screen and show game screen
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    this.update();
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+
+  update() {
+
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let startaGame;
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+    startaGame = new Game();
+    startaGame.start();
   }
 };
