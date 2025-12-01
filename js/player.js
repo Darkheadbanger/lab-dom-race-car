a3f8e631d8e7603b35dcc58a029cbd960f0e986a
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-01 14:46:02 +0700

    feat(player): implement movement and collision detection methods

--- js/player.js
+++ js/player.js
@@ -16,4 +16,48 @@ class Player {
     this.element.style.top = `${top}`;
     this.gameScreen.appendChild(this.element);
   }
+
+  move() {
+    this.directionX += this.left;
+    this.directionY += this.top;
+
+    if (this.left < 10) {
+      this.left = 10;
+    }
+
+    if (this.top < 10) {
+      this.top = 10;
+    }
+
+    if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+      this.left = this.gameScreen.offsetWidth - this.width - 10;
+    }
+
+    if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+      this.top = this.gameScreen.offsetHeight - this.height - 10;
+    }
+
+    this.updatePosition();
+  }
+
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+
+  didCollide(obstacle) {
+    const playerRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+
+    if (
+      playerRect.left < obstacleRect.right &&
+      playerRect.right > obstacleRect.left &&
+      playerRect.top < obstacleRect.bottom &&
+      playerRect.bottom > obstacleRect.top
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
